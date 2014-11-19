4b1129cb7b5663c869ee2d667b9029c1a3fc78c5
Dave Clayton <davedx@gmail.com>
2014-11-19 19:51:38 +0100

    Added migration, form and styling for edit profile. Moved vendor assets

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,4 +12,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def edit
     super
   end
+
+  private
+ 
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+ 
+  def account_update_params
+    params.require(:user).permit(:name, :address, :city, :country, :mobile, :facebook_url, :about, :email, :password, :password_confirmation, :current_password)
+  end
+
 end
