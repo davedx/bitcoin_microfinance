40b3a91ca9377dd024de8f8cf2ffc70ac02710ea
Dave Clayton <davedx@gmail.com>
2014-11-23 21:54:19 +0100

    Received forward address for payments from Blockchain. Also updated database with correct production db and username

--- app/controllers/users/funds_controller.rb
+++ app/controllers/users/funds_controller.rb
@@ -1,10 +1,11 @@
-#require 'blockchain'
+require 'blockchain'
 
 class Users::FundsController < ApplicationController
 	def index
 	end
 
 	def edit
-		resp = Blockchain::receive('16ysD5Bx69nNeXjh7VA9mwQ3cNg6hM5SWq', 'http://localhost')
+		resp = Blockchain::receive('16ysD5Bx69nNeXjh7VA9mwQ3cNg6hM5SWq', 'http://87.208.217.136/')
+		@input_address = resp.input_address
 	end
 end
