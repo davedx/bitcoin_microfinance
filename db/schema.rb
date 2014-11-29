a593c61022fb1149afc874296b3b5018abc1afe1
Dave Clayton <davedx@gmail.com>
2014-11-29 21:55:12 +0100

    Added lender_loan controller and view to create lender_loan. Need to hook up to balance transfer and triggering of loan status change if fully funded. Also need to link up users to lender_loans properly so we can show list of lenders of funded loans.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124114704) do
+ActiveRecord::Schema.define(version: 20141129200631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,10 +65,12 @@ ActiveRecord::Schema.define(version: 20141124114704) do
     t.integer  "lender_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "lender_loans", ["lender_id"], name: "index_lender_loans_on_lender_id", using: :btree
   add_index "lender_loans", ["loan_id"], name: "index_lender_loans_on_loan_id", using: :btree
+  add_index "lender_loans", ["user_id"], name: "index_lender_loans_on_user_id", using: :btree
 
   create_table "lenders", force: true do |t|
     t.string   "name"
